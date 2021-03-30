b9e544d0247375e86884f67ca21f6f8237d3f71f
Dbilochi <deeksha.bilochi@techverito.com>
2021-03-30 13:24:28 +0530

    remove unwanted folder structure
    lease enter the commit message for your changes. Lines starting

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get 'user/:id/invitations', to: 'events#user_invitations'
       get 'user/:id/events', to: 'events#user_events'
       put 'invite/:id/update-status', to: 'events#update_invitations'
+      resources :events
     end
   end
 end
